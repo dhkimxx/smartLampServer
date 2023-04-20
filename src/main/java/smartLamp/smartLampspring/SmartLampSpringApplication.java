94d905747d34fce765a91cb0335eeb8032844dd5
root <dhkimxx@naver.com>
2023-04-20 06:02:13 +0000

    initial commit

---
+++ src/main/java/smartLamp/smartLampspring/SmartLampSpringApplication.java
@@ -0,0 +1,13 @@
+package smartLamp.smartLampspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLampSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLampSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/smartLamp/smartLampspring/SmartLampSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package smartLamp.smartLampspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLampSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
