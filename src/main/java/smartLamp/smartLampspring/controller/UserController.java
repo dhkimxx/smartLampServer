ab81f7b8cd56f2cfbd703460a60ceaa4d96d92f8
dhkimxx <dhkimxx@naver.com>
2023-05-06 00:59:03 +0900

    user api

--- src/main/java/smartLamp/smartLampspring/controller/UserController.java
+++ src/main/java/smartLamp/smartLampspring/controller/UserController.java
@@ -13,21 +13,24 @@ import smartLamp.smartLampspring.repository.UserRepository;
 public class UserController {
     UserRepository userRepository = new MemoryUserRepository();
 
-    // 사용자 생성 API
-    @PostMapping("/users")
+    // 사용자 정보 등록 API
+    @PostMapping("/user")
     public ResponseEntity<Void> createUser(@RequestBody User user) {
         if (userRepository.containUserId(user.getUserId())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
         userRepository.save(user);
+        System.out.println("사용자 정보 등록");
+        System.out.println("user.toString() = " + user.toString());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    // 사용자 수정 API
-    @PutMapping("/users")
+    // 사용자 정보 수정 API
+    @PutMapping("/user")
     public ResponseEntity<Void> updateUser(@RequestBody User user) {
         if (userRepository.containUserId(user.getUserId())) {
             userRepository.save(user);
+            System.out.println("사용자 정보 수정");
             System.out.println("user.toString() = " + user.toString());
             return ResponseEntity.status(HttpStatus.CREATED).build();
         }
@@ -41,13 +44,14 @@ public class UserController {
             User storedUser = userRepository.findById(user.getUserId());
             if (storedUser.getUserPw().equals(user.getUserPw())) {
                 storedUser.setAuthenticated(true);
+                System.out.println("사용자 인증");
+                System.out.println("storedUser.toString() = " + storedUser.toString());
                 return ResponseEntity.ok(storedUser);
             }
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
-    // 사용자 로그아웃
     @DeleteMapping("/authenticate")
     public ResponseEntity<Void> logoutUser(@RequestBody User user) {
         if (userRepository.containUserId(user.getUserId())) {
@@ -58,17 +62,5 @@ public class UserController {
             }
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-    }
-
-    // 로그인 상태 확인 API
-    @GetMapping("/users/{userId}")
-    public ResponseEntity<User> getUser(@PathVariable String userId) {
-        if (userRepository.containUserId(userId)) {
-            User storedUser = userRepository.findById(userId);
-            if (storedUser.isAuthenticated()) {
-                return ResponseEntity.ok(storedUser);
-            }
-        }
-        return ResponseEntity.notFound().build();
     }
 }
