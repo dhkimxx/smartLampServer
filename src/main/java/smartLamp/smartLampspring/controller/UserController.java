5a7a5dcc46e79bd8e0e1867911260d6be9d3c3b9
dhkimxx <dhkimxx@naver.com>
2023-06-10 18:27:19 +0900

    refactor: user api exception

--- src/main/java/smartLamp/smartLampspring/controller/UserController.java
+++ src/main/java/smartLamp/smartLampspring/controller/UserController.java
@@ -6,39 +6,49 @@ import org.springframework.web.bind.annotation.*;
 import smartLamp.smartLampspring.dto.UserInfoDto;
 import smartLamp.smartLampspring.service.UserService;
 
+import javax.security.sasl.AuthenticationException;
+
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
     private final UserService userService;
 
-    private UserController(UserService userService){
+    private UserController(UserService userService) {
         this.userService = userService;
     }
 
     // 사용자 정보 등록 API
     @PostMapping
     public ResponseEntity<Void> createUser(@RequestBody UserInfoDto userInfoDto) {
-        if(userService.create(userInfoDto)){
+        try {
+            userService.register(userInfoDto);
             return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
-        return ResponseEntity.status(HttpStatus.CONFLICT).build();
     }
 
     // 사용자 인증 API
     @PostMapping("/authenticate")
     public ResponseEntity<UserInfoDto> authenticateUser(@RequestBody UserInfoDto userInfoDto) {
-        if (userService.login(userInfoDto).isPresent()){
-                return ResponseEntity.ok(userService.login(userInfoDto).get());
-            }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        try {
+            UserInfoDto userInfo = userService.login(userInfoDto);
+            return ResponseEntity.ok(userInfo);
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
     }
 
     // 사용자 로그아웃 API
     @DeleteMapping("/authenticate")
     public ResponseEntity<Void> logoutUser(@RequestBody UserInfoDto userInfoDto) {
-        if(userService.logout(userInfoDto)){
-                return ResponseEntity.ok().build();
+        try {
+            userService.logout(userInfoDto);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
     }
 }
--- src/main/java/smartLamp/smartLampspring/service/UserService.java
+++ src/main/java/smartLamp/smartLampspring/service/UserService.java
@@ -1,13 +1,15 @@
 package smartLamp.smartLampspring.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.transaction.annotation.Transactional;
 import smartLamp.smartLampspring.dto.UserInfoDto;
 import smartLamp.smartLampspring.Entity.User;
 import smartLamp.smartLampspring.repository.UserRepository;
 
+import javax.security.sasl.AuthenticationException;
+import java.util.NoSuchElementException;
 import java.util.Objects;
-import java.util.Optional;
 
 @Transactional
 public class UserService {
@@ -18,62 +20,38 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public boolean logout(UserInfoDto userInfoDto) {
-        Optional<User> storedUser = userRepository.findById(userInfoDto.getUserId());
-        if (storedUser.isPresent()) {
-            storedUser.get().setAuthenticated(false);
-            userRepository.save(storedUser.get());
-            return true;
-        }
-        return false;
+    public void logout(UserInfoDto userInfoDto) {
+        User storedUser = userRepository.findById(userInfoDto.getUserId())
+                .orElseThrow(() -> new NoSuchElementException("No user found"));
+
+        storedUser.setAuthenticated(false);
+        userRepository.save(storedUser);
     }
 
-    public Optional<UserInfoDto> login(UserInfoDto userInfoDto) {
-        Optional<User> storedUser = userRepository.findById(userInfoDto.getUserId());
-        if (storedUser.isPresent()) {
-            if (Objects.equals(storedUser.get().getUserPw(), userInfoDto.getUserPw())) {
-                storedUser.get().setAuthenticated(true);
-                userRepository.save(storedUser.get());
-                userInfoDto.setUserName(storedUser.get().getUserName());
-                return Optional.of(userInfoDto);
-            }
-            return Optional.empty();
+    public UserInfoDto login(UserInfoDto userInfoDto) throws AuthenticationException {
+        User storedUser = userRepository.findById(userInfoDto.getUserId())
+                .orElseThrow(() -> new NoSuchElementException("No user found"));
+
+        if (!Objects.equals(storedUser.getUserPw(), userInfoDto.getUserPw())) {
+            throw new AuthenticationException("Invalid password");
         }
-        return Optional.empty();
+
+        storedUser.setAuthenticated(true);
+        userRepository.save(storedUser);
+        userInfoDto.setUserName(storedUser.getUserName());
+        return userInfoDto;
     }
 
-    public boolean create(UserInfoDto userInfoDto) {
-        Optional<User> storedUser = userRepository.findById(userInfoDto.getUserId());
-        if (storedUser.isPresent()) {
-            return false;
-        }
+    public void register(UserInfoDto userInfoDto) {
+        userRepository.findById(userInfoDto.getUserId()).ifPresent(user -> {
+            throw new DataIntegrityViolationException("userId already exists");
+        });
+
         User newUser = new User();
         newUser.setUserId(userInfoDto.getUserId());
         newUser.setUserPw(userInfoDto.getUserPw());
         newUser.setUserName(userInfoDto.getUserName());
         userRepository.save(newUser);
-        return true;
     }
 
-    public Optional<User> read(User user) {
-        return userRepository.findById(user.getUserId());
-    }
-
-    public boolean update(User user) {
-        Optional<User> storedUser = userRepository.findById(user.getUserId());
-        if (storedUser.isPresent()) {
-            userRepository.save(user);
-            return true;
-        }
-        return false;
-    }
-
-    public boolean delete(User user) {
-        Optional<User> storedUser = userRepository.findById(user.getUserId());
-        if (storedUser.isPresent()) {
-            userRepository.delete(user);
-            return true;
-        }
-        return false;
-    }
 }
